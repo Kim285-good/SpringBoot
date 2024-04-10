81aeef2d7367f4d775e86b0a6abd655c0fa917b5
Dohwan Kim <bar0805@naver.com>
2024-04-10 19:26:18 +0900

    재고관리 프로그램

---
+++ src/main/java/kr/co/hanbit/product_management/domain/Product.java
@@ -0,0 +1,44 @@
+package kr.co.hanbit.product_management.domain;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+import kr.co.hanbit.product_management.presentation.ProductDto;
+
+import java.util.Objects;
+
+public class Product {
+
+    private Long id;
+
+    @Size(min = 1, max = 100)
+    private String name;
+
+    @Max(1000000)
+    @Min(0)
+    private Integer price;
+
+    @Max(9999)
+    @Min(0)
+    private Integer amount;
+
+    public void setId(Long id) {
+        this.id = id;
+        }
+
+    public Boolean sameId(Long id) {
+        return this.id.equals(id);
+    }
+
+    public Boolean containsName(String name) {
+        return this.name.contains(name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+}
