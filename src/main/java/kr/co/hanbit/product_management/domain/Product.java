b932cf1329b113b2b2a8eedf275a69c733ae4ee4
Dohwan Kim <bar0805@naver.com>
2024-05-29 13:15:20 +0900

    수정 삭제 메소드 구현

--- src/main/java/kr/co/hanbit/product_management/domain/Product.java
+++ src/main/java/kr/co/hanbit/product_management/domain/Product.java
@@ -22,6 +22,10 @@ public class Product {
     @Min(0)
     private Integer amount;
 
+    public Long getId() {
+        return id;
+    }
+
     public String getName() {
         return name;
     }
@@ -37,6 +41,18 @@ public class Product {
     public void setId(Long id) {
         this.id = id;
         }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
 
     public Boolean sameId(Long id) {
         return this.id.equals(id);
